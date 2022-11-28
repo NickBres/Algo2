f61866956f84246b2307cfc1e09545e6529eeaf2
NickBres <70432147+NickBres@users.noreply.github.com>
2022-11-28 18:13:54 +0200

    EX2 now working correct!

--- Ex2.py
+++ Ex2.py
@@ -1,44 +1,78 @@
 class City:
     def __init__(self, houses_prices):
+        self.size = len(houses_prices)
         self.houses_prices = houses_prices
+        self.to_repair = self.size * [False]
+        self.price = self.bestPrice()
 
     # O(n)
     def bestPrice(self):
-        total = 0
-        is_visited = [0] * len(self.houses_prices)
-        count = 0
-
-        while count < len(self.houses_prices):
-            max_index = self.findMaxNotVisited(is_visited)
-            sum_of_neighbours = 0
-            is_visited[max_index] = 1
-            if max_index - 1 >= 0 and is_visited[max_index - 1] == 0:
-                is_visited[max_index - 1] = 1
-                sum_of_neighbours += self.houses_prices[max_index - 1]
-                count += 1
-            if max_index + 1 < len(self.houses_prices) and is_visited[max_index + 1] == 0:
-                is_visited[max_index + 1] = 1
-                sum_of_neighbours += self.houses_prices[max_index + 1]
-                count += 1
-            if sum_of_neighbours < self.houses_prices[max_index]:
-                total += sum_of_neighbours
-            else:
-                total += self.houses_prices[max_index]
-            count += 1
-
-        return total
-
-    def findMaxNotVisited(self, is_visited):
         i = 0
-        while i < len(is_visited) and is_visited[i] == 1:
-            i += 1
-        if i == len(is_visited):
-            return -1
-        max_index = i
-        while i < len(is_visited):
-            if is_visited[i] == 0 and self.houses_prices[max_index] < self.houses_prices[i]:
-                max_index = i
-            i += 1
+        j = 1
+        while j < self.size:
+            if self.houses_prices[i] <= self.houses_prices[j]:
+                self.to_repair[i] = True
+            else:
+                self.to_repair[j] = True
+            i += 2
+            j += 2
+        gap_index = self.findGap()
+        while gap_index != -1:
+            new_sum_from_left = 0
+            i = gap_index
+            j = i - 1
+            while i >= 0 and not (self.to_repair[i]):
+                new_sum_from_left += self.houses_prices[i]
+                if j >= 0:
+                    new_sum_from_left -= self.houses_prices[j]
+                i -= 2
+                j -= 2
 
-        return max_index
+            new_sum_from_right = 0
+            i = gap_index + 1
+            j = i + 1
+            while i < self.size and not (self.to_repair[i]):
+                new_sum_from_right += self.houses_prices[i]
+                if j < self.size:
+                    new_sum_from_right -= self.houses_prices[j]
+                i += 2
+                j += 2
 
+            if new_sum_from_right < new_sum_from_left:
+                i = gap_index + 1
+                j = i + 1
+                while i < self.size and not (self.to_repair[i]):
+                    self.to_repair[i] = True
+                    if j < self.size:
+                        self.to_repair[j] = False
+                    i += 2
+                    j += 2
+            else:
+                i = gap_index
+                j = i - 1
+                while i >= 0 and not (self.to_repair[i]):
+                    self.to_repair[i] = True
+                    if j >= 0:
+                        self.to_repair[j] = False
+                    i -= 2
+                    j -= 2
+            gap_index = self.findGap()
+
+        return self.calcTotal()
+
+    def findGap(self):
+        i = 0
+        j = 1
+        while j < self.size:
+            if not (self.to_repair[i]) and not (self.to_repair[j]):
+                return i
+            i += 1
+            j += 1
+        return -1
+
+    def calcTotal(self):
+        total = 0
+        for index, flag in enumerate(self.to_repair):
+            if flag:
+                total += self.houses_prices[index]
+        return total
--- main.py
+++ main.py
@@ -4,18 +4,21 @@ import Ex3
 import Ex4
 
 if __name__ == '__main__':
-
     heights = [0, 0, 1, 0, 0, 3, 5, 6, 5, 4, 1, 3, 2, 2, 4, 6, 10, 9, 9, 11, 8, 3, 5, 4, 4, 6, 2, 0, 0, 1, 1, 0, 3, 3,
                2, 0, 0, 1]
     ex1 = Ex1.Hills(heights)
     print("1: " + str(ex1.waterLevel()))
 
-    prices1 = [800, 1000, 900]
+    prices1 = [50, 30, 40, 60, 10, 30, 10]
     ex2 = Ex2.City(prices1)
-    print("2: " + str(ex2.bestPrice()))
+    print("2: " + str(ex2.price))
+    print(prices1)
+    print(ex2.to_repair)
     prices2 = [30, 50, 60, 40, 100000000000, 1000, 20, 30]
     ex2 = Ex2.City(prices2)
-    print("2: " + str(ex2.bestPrice()))
+    print("2: " + str(ex2.price))
+    print(prices2)
+    print(ex2.to_repair)
 
     words = ["hell", "hello", "low", "world", "my", "name", "is", "nick"]
     ex3 = Ex3.Lang(words)
